ef91ec970b29e9f41a3ec33f13cfe0ca0666e3c9
Mohamed Ashour <97407829+MohamedAdelll@users.noreply.github.com>
2024-10-23 18:47:06 +0300

    final changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
+const backgroundHeroStyle = {
+  clipPath:
+    "polygon(0% 0%, 100% 0%, 100% 90%, 95% 100%, 5% 100%, 0% 90%, 0% 0%)",
+};
+const polygonStyle = {
+  clipPath: "polygon(5% 0%, 95% 0%, 100% 50%, 95% 100%, 5% 100%, 0% 50%)",
+};
 function App() {
   return (
-    <section className="h-[600px] bg-custom-gradient relative border-[rgba(255,255,255,0.1)]">
-      <div className="polygon-container">
-        <div className="absolute inset-0 opacity-50">
+    <>
+      <section className="h-[600px] relative overflow-visible">
+        <div
+          className="absolute bg-custom-gradient w-full"
+          style={backgroundHeroStyle}
+        >
           <img
             src="/public/section-bg.jpeg"
             alt="Background"
-            className="w-full h-full object-cover"
+            className="opacity-50 w-full h-full object-contain"
           />
         </div>
-      </div>
-    </section>
+        <div
+          className="absolute bg-[#8858B5] bottom-[2px] text-[40px] left-1/2 -translate-x-1/2 translate-y-1/2 flex justify-center items-center px-6 py-5"
+          style={polygonStyle}
+        >
+          <span className="font-semibold text-white">Choose your sphere</span>
+        </div>
+      </section>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,13 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'custom-gradient': 'linear-gradient(180deg, #000000 13.92%, #8858B5 100%)',
-      }
-    }
-      
+        "custom-gradient":
+          "linear-gradient(180deg, #000000 13.92%, #8858B5 100%)",
+      },
+    },
+    boxShadow: {
+      "custom-black": "0px 100px 100px 0px rgba(0, 0, 0, 1)",
+    },
   },
   plugins: [],
-}
-
+};
